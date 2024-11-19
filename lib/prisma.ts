5cf3487b07a8fbea96666c1932f01de9ddd74444
harshith <harshith2742@gmail.com>
2024-11-19 23:16:13 +0530

    updates prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+
+// Ensure a single instance of PrismaClient in development
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // Enable detailed logging for debugging
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
