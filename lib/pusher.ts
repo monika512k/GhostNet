03399799a5fd6d1759fb85370eb01bc372d82683
harshith <harshith2742@gmail.com>
2024-11-22 10:09:09 +0530

    updated pusher.ts

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -4,13 +4,13 @@ import PusherClient from 'pusher-js';
 
 export const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!, // Changed to NEXT_PUBLIC_PUSHER_KEY
   secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!, // Changed to NEXT_PUBLIC_PUSHER_CLUSTER
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-  // encrypted: true
+  forceTLS: true  // Changed from encrypted
 });
